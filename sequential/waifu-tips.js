627ad58647980e14eeda2c3a1e4c8cf8c41d1414
AdingApkgg <AdingApkgg@outlook.com>
2023-08-09 04:28:59 +0800

    .

--- sequential/waifu-tips.js
+++ sequential/waifu-tips.js
@@ -1,11 +1,3 @@
-/*
- * @Author: AdingApkgg AdingApkgg@outlook.com
- * @Date: 2023-08-07 01:01:25
- * @LastEditors: AdingApkgg AdingApkgg@outlook.com
- * @LastEditTime: 2023-08-09 04:27:19
- * @FilePath: \live2d-api\sequential\waifu-tips.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /*!
  * Live2D APi
  * https://github.com/nova1751/live2d-api
