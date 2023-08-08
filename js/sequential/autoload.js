7ea3f90df95ba1fe63cb7d721cfa08f3efde47a7
AdingApkgg <AdingApkgg@outlook.com>
2023-08-09 03:25:57 +0800

    .

--- js/sequential/autoload.js
+++ js/sequential/autoload.js
@@ -2,7 +2,7 @@
 // const live2d_path =
 //   "https://fastly.jsdelivr.net/gh/nova1751/live2d-widget@latest/";
 
-const live2d_path = "https://cdn.staticaly.com/gh/adingapkgg/live2d-api@main/";
+const live2d_path = "";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -29,14 +29,14 @@ function loadExternalResource(url, type) {
 if (screen.width >= 768) {
   Promise.all([
     loadExternalResource(live2d_path + "live2d.min.js", "js"),
-    loadExternalResource(live2d_path + "npm/random/waifu-tips.js", "js"),
+    loadExternalResource(live2d_path + "js/sequential/waifu-tips.js", "js"),
   ]).then(() => {
     // 配置选项的具体用法见 README.md
     initWidget({
       waifuPath: live2d_path + "waifu-tips.json",
 
       // apiPath: "https://live2d.fghrsh.net/api/",
-      cdnPath: "https://cdn.staticaly.com/gh/adingapkgg/live2d-api@main/",
+      cdnPath: "",
 
       tools: [
         "hitokoto",
